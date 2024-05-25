3c4151e24c43194162a702657dc2123985c4e0ce
Federico Gerardi <38018471+iGieri@users.noreply.github.com>
2024-05-26 00:25:57 +0200

    fixed bugs + modified cansat

--- js/projects.js
+++ js/projects.js
@@ -16,10 +16,10 @@ fetch('../data.json')
       projects += `<div class="col-md-4">
                     <div class="card m-3">
                         <img src="${project.logo}" class="card-img-top" alt="${project.name}">
-                        <div class="card-body">
+                        <div class="card-body" style="text-align: center;">
                             <h5 class="card-title">${project.name}</h5>
                             <p class="card-text">${project.short_description}</p>
-                            <button onclick="showProject(\`${project.name}\`, \`${project.logo}\`, \`${project.description}\`)" class="btn btn-primary">Learn More</button>
+                            <button onclick="showProject(\`${project.name}\`, \`${project.logo}\`, \`${project.description}\`)" class="btn btn-dark link_button">Learn More</button>
                         </div>
                     </div>
                 </div>`;
